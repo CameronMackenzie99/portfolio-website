9f8d35eb8b16acdb8d35b3cafd809ecde15920ec
CameronMackenzie99 <mackencameron@gmail.com>
2022-03-07 18:47:35 +0000

    Add footer to layout

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -1,3 +1,4 @@
+import { Footer } from '../components/Footer'
 import Nav from '../components/Nav'
 
 export function Layout(props) {
@@ -5,6 +6,7 @@ export function Layout(props) {
     <div className="bg-gradient-to-tr from-slate-200 via-slate-100 to-slate-300 dark:from-slate-800 dark:via-slate-700 dark:to-sky-900 pb-4 h-screen bg-cover text-slate-900 dark:text-slate-200">
       <Nav />
       {props.children}
+      <Footer />
     </div>
   )
 }
